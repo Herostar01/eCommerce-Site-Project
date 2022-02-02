91db3cda061894841fad6d63bd5843239c43803d
Muhammad Shahbaz <M023Shah@gmail.com>
2022-02-01 19:50:44 -0800

    pseudo products added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
 
       <Container>
       <h1>Welcome to Tech ProShop</h1>
+      <HomeScreen />
       </Container>
 
       </main>
