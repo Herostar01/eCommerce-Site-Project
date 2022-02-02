eb46719f83300961c9e47b9190f35804de89ed00
Muhammad Shahbaz <M023Shah@gmail.com>
2022-02-01 19:13:33 -0800

    navbar added

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div >
+
+      <Header />
+
+      <main className='py-3'>
+
+      <Container>
+      <h1>Welcome to Tech ProShop</h1>
+      </Container>
+
+      </main>
+
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
