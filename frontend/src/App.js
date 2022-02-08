7c62f42e678abe22d90c35da1fa72a6b7fbe8cfd
Muhammad Shahbaz <M023Shah@gmail.com>
2022-02-08 13:23:32 -0800

    adding react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
   return (
-    <div >
+
+    <Router>
 
       <Header />
 
@@ -15,15 +18,17 @@ function App() {
 
       <Container>
       <h1>Welcome to Tech ProShop</h1>
-      <HomeScreen />
+      <Route path='/' component={HomeScreen} exact />
+      <Route path='/product/:id' component={ProductScreen} />
       </Container>
 
       </main>
 
       <Footer />
       
-    </div>
-  );
+    
+    </Router>
+  )
 }
 
 export default App;
