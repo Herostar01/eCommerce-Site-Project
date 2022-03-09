214dcf4f6f03aed7398d3bbea2403db36779c7c2
Muhammad Shahbaz <M023Shah@gmail.com>
2022-03-08 19:17:06 -0800

    finished backend user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 
@@ -6,7 +7,7 @@ import User from '../models/userModel.js'
 // @desc Authenticate User, Gets Token
 // @route GET /api/users/login
 // @access Public
-export const authUser = asyncHandler(async(req, res) => {
+const authUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body 
 
     const user = await User.findOne({ email })
@@ -14,13 +15,78 @@ export const authUser = asyncHandler(async(req, res) => {
     if(user  && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
+            name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
         throw new Error('Invalid email or password')
     }
 })
+
+
+// @desc Register a new User
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body 
+
+    const userExists = await User.findOne({ email })
+
+    if(userExists) {
+        res.status(400)
+        throw new Error("User Already Exists")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid User data")
+
+    }
+})
+
+
+// USER Route to get profile
+// @desc Gets Profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+
+export {
+    authUser,
+    getUserProfile,
+    registerUser
+}
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,15 @@
 import express from 'express'
 const router = express.Router()
 
-import {authUser} from '../controllers/userController.js'
+import {authUser, getUserProfile, registerUser} from '../controllers/userController.js'
+import {protect} from '../middleware/authMiddleware.js'
 
 
-
-// Gets all products from database
-// @desc Get all products
-// @route GET /api/products
-// @access Public
+router.route('/').post(registerUser)
 
 router.post('/login', authUser)
+
+router.route('/profile').get(protect, getUserProfile) 
 
 
 
