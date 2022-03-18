31a8f72904f2b6c1a12a017f653d9186a20ae890
Muhammad Shahbaz <M023Shah@gmail.com>
2022-03-18 16:34:44 -0700

    adding profile screen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express'
 const router = express.Router()
 
-import {authUser, getUserProfile, registerUser} from '../controllers/userController.js'
+import {authUser, 
+    getUserProfile, 
+    registerUser,
+    updateUserProfile
+} from '../controllers/userController.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 
@@ -9,7 +13,11 @@ router.route('/').post(registerUser)
 
 router.post('/login', authUser)
 
-router.route('/profile').get(protect, getUserProfile) 
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
+
 
 
 
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,7 +4,10 @@ import { USER_LOGIN_FAIL,
     USER_LOGOUT,
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
-    USER_REGISTER_FAIL} from "../constants/userConstants"
+    USER_REGISTER_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL} from "../constants/userConstants"
 import axios from 'axios'
 
 export const login = (email, password) => async (dispatch) => {
@@ -85,3 +88,41 @@ export const register = (name, email, password) => async (dispatch) => {
     }
 
 }
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: USER_DETAILS_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.get(`/api/users/${id}`, config)
+  
+      dispatch({
+        type: USER_DETAILS_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: USER_DETAILS_FAIL,
+        payload: message,
+      })
+    }
+  }
+  
